a9ea5bcbea9a18527b93336819a30a8d40570d33
pgltsmn <93178841+pgltsmn@users.noreply.github.com>
2022-08-04 01:11:27 +0300

    hooy

--- Individual-Project/app.py
+++ Individual-Project/app.py
@@ -5,12 +5,14 @@ import pyrebase
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
-#Code goes below here
 
+@app.route('/')
+def index():
+  return render_template('index.html')
 
-
-
-#Code goes above here
+@app.route('/signin')
+def signin():
+  return render_template('signin.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
